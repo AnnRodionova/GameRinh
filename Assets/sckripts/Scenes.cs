f0cae76473b8998b53b958b8e705a4933f9b4443
AnnRodionova <83912150+AnnRodionova@users.noreply.github.com>
2021-06-26 01:51:19 +0300

    Add files via upload

---
+++ Assets/sckripts/Scenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+   public void ChangeScenes()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Игра закрылась");
+        Application.Quit();
+    }
+}
